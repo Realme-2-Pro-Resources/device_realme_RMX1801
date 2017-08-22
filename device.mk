d0974f5606d5984db6b7e77dd98d5074ab8d1897
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:58 +0200

    RMX1801: Remove unused libqsap_sdk
    Change-Id: Ib543425786c498eec8ef421f85047bcb1cb0cae3
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -457,7 +457,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    libqsap_sdk \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant \
