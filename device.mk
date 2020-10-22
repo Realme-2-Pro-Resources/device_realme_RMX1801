da21316ca392bfc54cfc16dfd669ac23ad05c429
Ethan Chen <intervigil@gmail.com>
2020-10-22 14:38:37 +0530

    RMX1801: Convert libhidl to blueprint
    Change-Id: Ief105a44eeb403b2fa41d391a0d72c776da13248

--- device.mk
+++ device.mk
@@ -245,7 +245,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor
+    android.hidl.base@1.0.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
