30d2cd16935deeabefeb274002a7d51712163d5c
BeYkeRYkt <beykerykt@gmail.com>
2021-03-29 04:30:13 +0900

    RMX1801: Drop textclassifier
    * Target no longer exists
    
    Change-Id: Ibd08dcd9ebec5dfde7935b0f3494bdbf5a3a1d20

--- device.mk
+++ device.mk
@@ -397,10 +397,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
