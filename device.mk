4dcf77fec0f65aa773aa43230a2a94bf4fa54892
Tim Zimmermann <tim@linux4.de>
2021-07-26 21:13:30 +0530

    RMX1801: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Change-Id: Icbc828389465f3d4681ca8e4c3adec782540752e
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -367,6 +367,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # RealmeParts
 PRODUCT_PACKAGES += \
     RealmeParts
