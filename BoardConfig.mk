04ec8ed2dffd358810c32a9060deae44439667df
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 19:25:21 +0200

    RMX1801: Remove BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED
    * Defaults to true if full VNDK is enabled
    
    Change-Id: I2d59cbae13eb3712fd13ab09eea582c8d5768e8f
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
