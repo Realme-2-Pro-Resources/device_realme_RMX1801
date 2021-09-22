bfbf7f965d3bae3e8ba9806f64477aa44c70fd1d
ZVNexus <akoskovich@pm.me>
2021-09-22 17:24:27 +0200

    RMX1801: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: Ia2cb9394fdc65148100032620aabc306f7b3c1e7

--- device.mk
+++ device.mk
@@ -373,10 +373,6 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_RMX1801
