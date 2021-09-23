69abb3b1171610dd5895487c4ec3f580496c3e3a
LuK1337 <priv.luk@gmail.com>
2021-09-23 12:46:33 +0200

    RMX1801: touch: Allow devices to override gestures list
    SDM660 devices had more gestures available than anything after it.
    
    Change-Id: I7d74d870ec8cbb80b24c690d481865e317a0b6cb

--- touch/TouchscreenGesture.cpp
+++ touch/TouchscreenGesture.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #define LOG_TAG "TouchscreenGestureService"
 
 #include "TouchscreenGesture.h"
+#include "touch/realme/TouchscreenGesture.h"
 #include <android-base/logging.h>
 #include <fstream>
 
@@ -25,21 +26,6 @@ namespace lineage {
 namespace touch {
 namespace V1_0 {
 namespace implementation {
-
-const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
-    {0, {250, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
-    {1, {251, "Up arrow", "/proc/touchpanel/up_arrow_enable"}},
-    {2, {252, "Down arrow", "/proc/touchpanel/down_arrow_enable"}},
-    {3, {253, "Left arrow", "/proc/touchpanel/left_arrow_enable"}},
-    {4, {254, "Right arrow", "/proc/touchpanel/right_arrow_enable"}},
-    {5, {63, "One finger up swipe", "/proc/touchpanel/up_swipe_enable"}},
-    {6, {64, "One finger down swipe", "/proc/touchpanel/down_swipe_enable"}},
-    {7, {65, "One finger left swipe", "/proc/touchpanel/left_swipe_enable"}},
-    {8, {66, "One finger right swipe", "/proc/touchpanel/right_swipe_enable"}},
-    {9, {247, "Letter M", "/proc/touchpanel/letter_m_enable"}},
-    {10, {249, "Letter O", "/proc/touchpanel/letter_o_enable"}},
-    {11, {246, "Letter W", "/proc/touchpanel/letter_w_enable"}},
-};
 
 Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
     std::vector<Gesture> gestures;
