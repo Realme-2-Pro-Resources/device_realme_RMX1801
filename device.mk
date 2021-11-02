edb81b0c004f34736a19df7bc4186921f4d0295f
Jake Weinstein <jake@aospa.co>
2021-11-02 21:50:59 +0530

    RMX1801: Use speed tuning for performance critical applications
    The default ART setting is quicken: run DEX code verification and
    optimize some DEX instructions to get better interpreter performance.
    
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    Change-Id: Ib2dc9a55dfb332ba3fb84a13544d564a63f37ad2
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -407,6 +407,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 WITH_DEXPREOPT_DEBUG_INFO := false
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
+
 # Telephony-ext
 PRODUCT_PACKAGES += \
     ims-ext-common \
