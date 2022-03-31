01d3034386c8fe4cc5f7183fd8e1479fba5e99e1
LuK1337 <priv.luk@gmail.com>
2022-03-31 18:05:37 +0200

    Revert "RMX1801: Make Android.mk search recursive"
    No longer needed.
    
    This reverts commit 1eaddf8978ad4c6beb68f131b57a651bc729ebcc.
    
    Change-Id: Iae9c4257a98dd4ba47ef9992b1dec133ccc56764
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- Android.mk
+++ Android.mk
@@ -21,8 +21,8 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1801)
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
