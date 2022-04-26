6887b0b03e29eb877e63247ff6633d888231f155
chandu078 <chandudyavanapelli03@gmail.com>
2022-04-26 21:12:21 +0530

    RMX1801: Drop mock hidl power stats
    - E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData
    
    This reverts commit a6d8e4f.
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -332,8 +332,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.realme_sdm660-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.realme_sdm660-libperfmgr
 
 # Powerhint
 PRODUCT_COPY_FILES += \
