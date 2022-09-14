ad862b59a761677e22ebe68fde559187b6c054af
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    RMX1801: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -197,7 +197,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
