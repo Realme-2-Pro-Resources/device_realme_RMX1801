1868ef7968cfd01a32a6a34591ed463803ed9eee
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-10-27 13:01:38 +0530

    Revert "RMX1801: Switch BtAudio to AIDL"
    Breaks BT here
    
    https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sdm845-common/+/338951/comments/adae79f1_315e2e92
    https://review.lineageos.org/c/LineageOS/android_device_oneplus_sm8350-common/+/338968
    
    This reverts commit e004239ca3da137d642649ca21f1f077b8c0c54d.
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
     libbluetooth_audio_session \
     vendor.qti.hardware.btconfigstore@1.0.vendor
