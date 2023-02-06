d17ecd76d17d3ba2761e3e078a2f5f94ee6e9833
Alexander Koskovich <akoskovich@pm.me>
2023-02-06 19:00:13 +0100

    RMX1801: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -164,7 +164,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6:64 \
     libstdc++.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0
 
 # Configstore
