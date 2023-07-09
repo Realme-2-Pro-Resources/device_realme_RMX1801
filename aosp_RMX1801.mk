f8344b4de69623394d3b41d962b406dbe2d71847
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-07-09 02:37:20 +0000

    RMX1801: Define boot animation resolution
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- aosp_RMX1801.mk
+++ aosp_RMX1801.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOS stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from Realme RMX1801
 $(call inherit-product, device/realme/RMX1801/device.mk)
