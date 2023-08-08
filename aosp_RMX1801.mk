ebc573ae263c58b01b747ac3065efcf01ee8d7f1
Juhyung Park <qkrwngud825@gmail.com>
2023-08-08 22:06:15 +0900

    RMX1801: stop shipping 32-bit Zygote
    Pixel 7 onwards also do the same, meaning that the user inconvenience is
    none if not minimal.
    
    This saves non-negligible amount of RAM.
    
    Change-Id: Ide8671817d15e79ab61a1665cf3f60e599ff3981
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- aosp_RMX1801.mk
+++ aosp_RMX1801.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit some common AOSP stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOS stuff
