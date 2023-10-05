a6c7a873c394f94a101842c5670ead2ea873946d
me-cafebabe <me.cafebabe@gmail.com>
2023-10-06 04:32:27 +0800

    RMX1801: libstdc++.vendor => libstdc++_vendor
    Change-Id: I478eb89aa2ac106d855b168844863032612bc8e8
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -64,6 +64,9 @@ function blob_fixup() {
         vendor/lib/hw/camera.sdm660.so|vendor/lib/libarcvs.dep.so|vendor/lib/libmmcamera2_sensor_modules.so|vendor/lib/libmmcamera_interface.so)
             sed -i 's|/system/etc/camera/|/vendor/etc/camera/|g' "${2}"
             ;;
+        vendor/lib/libSonyIMX519PdafLibrary.so|vendor/lib/libPerfectlyClearCrux.so|vendor/lib/libarcsoft_dualcam_bokeh_api.so|vendor/lib/libarcsoft_dualcam_refocus_left.so|vendor/lib/libmmcamera_hdr_gb_lib.so|vendor/lib/libdepthmap.so|vendor/lib/libarcsoft_dualcam_refocus_right.so|vendor/lib/libarcsoft_dualcam_refocus_preview.so|vendor/lib/libfilter.so|vendor/lib/libSonyIMX376RmscLibrary.so|vendor/lib/libarcsoft_smart_denoise.so|vendor/lib/libarcsoft_dualcam_refocus.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
     esac
 }
 
