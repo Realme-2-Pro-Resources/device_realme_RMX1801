7cb814a97ee8eede6b898c5eede359e64f037eba
me-cafebabe <me.cafebabe@gmail.com>
2023-10-06 04:32:27 +0800

    RMX1801: libstdc++.vendor => libstdc++_vendor
    Change-Id: I478eb89aa2ac106d855b168844863032612bc8e8
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.6:64 \
-    libstdc++.vendor \
+    libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0
 
 # ConfigStore
