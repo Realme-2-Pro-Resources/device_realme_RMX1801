3e711c2575ee9882301dea4a8479ed3279581a1a
Gabriel Biren <gbiren@google.com>
2023-10-09 10:56:54 -0700

    RMX1801: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -456,7 +456,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
