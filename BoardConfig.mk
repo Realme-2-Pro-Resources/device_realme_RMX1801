3956e79e2eddaa053ba486166aa46866fb248a2c
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    RMX1801: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ TARGET_USES_ION := true
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE   := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_realme
