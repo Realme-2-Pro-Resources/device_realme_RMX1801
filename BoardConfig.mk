8d674b13e656307e0a54f6a5ae5cac3fbdb5121f
dodyirawan85 <dev.irawans@gmail.com>
2024-04-17 11:52:53 +0700

    [TMP] RMX1801: Bringup permissive
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_PAGE_SIZE := 4096
 BOARD_RAMDISK_OFFSET     := 0x01000000
