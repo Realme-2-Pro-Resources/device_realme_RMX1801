ffe3487bab72788f217ba151b1583cce613d2e42
Bruno Martins <bgcngm@gmail.com>
2024-07-10 09:22:00 +0700

    RMX1801: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- device.mk
+++ device.mk
@@ -432,3 +432,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
+
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
