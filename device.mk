ef8c79140430c1633bcb75444fcd5f32dc3eb232
Cosmin Tanislav <demonsingur@gmail.com>
2024-07-29 23:34:33 +0300

    RMX1801: build libstagefright_softomx_plugin
    vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found
    
    Change-Id: I56b620ebb9f9dc64ecc492bdb7ee17c3df57e593

--- device.mk
+++ device.mk
@@ -297,7 +297,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
