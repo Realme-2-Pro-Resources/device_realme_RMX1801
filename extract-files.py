2d894232131cac6bbf97a5775ce7fa93e89be052
Marc Bourgoin <themard85@gmail.com>
2024-11-17 17:51:59 -0700

    RMX1801: Setup ELF checks & regenerate vendor
    According to templates for py extract-utils
    
    https://github.com/LineageOS/android_tools_extract-utils/tree/lineage-22.0/templates
    Change-Id: Ibb1cef7c9492c091f1626f3c4623ad0cd3e7b708

--- extract-files.py
+++ extract-files.py
@@ -12,10 +12,34 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/realme/RMX1801',
+    'hardware/qcom-caf/msm8998',
+    'hardware/qcom-caf/wlan',
+    'hardware/oppo',
+    'vendor/qcom/opensource/dataservices',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
@@ -48,6 +72,8 @@ module = ExtractUtilsModule(
     'RMX1801',
     'realme',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
