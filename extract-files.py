00852c4968cf0688a52ffdc27b8d4cf213043b41
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-29 13:49:19 -0500

    RMX1801: fix ELF checks for vendor.qti.hardware.qccsyshal@1.0
    Change-Id: Ie265887b86462ab2af6f1d8a475211b14c5e62a4

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,7 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.uceservice@2.2',
         'com.qualcomm.qti.uceservice@2.3',
         'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
