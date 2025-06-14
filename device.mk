8e4bea4220363d12fdf485ec3715e247d45bf769
dodyirawan85 <dev.irawans@gmail.com>
2025-06-14 10:23:26 +0700

    RMX1801: Drop non vendorcompat libprotobuf-cpp-full
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -357,7 +357,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
-    libprotobuf-cpp-full \
     librmnetctl \
     libxml2  \
     qti-telephony-hidl-wrapper \
