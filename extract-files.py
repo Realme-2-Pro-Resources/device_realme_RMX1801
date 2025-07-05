b5b84ad36f12853dd53cc138961da18c8b0ecc2f
Michael Bestas <mkbestas@lineageos.org>
2025-07-05 21:06:46 +0700

    RMX1801: Switch to python extract-utils
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
+    (
+        'vendor/lib/hw/camera.sdm660.so',
+        'vendor/lib/libarcvs.dep.so',
+        'vendor/lib/libmmcamera2_sensor_modules.so',
+        'vendor/lib/libmmcamera_interface.so',
+    ): blob_fixup()
+        .binary_regex_replace(b'/system/etc/camera/', b'/vendor/etc/camera/'),
+    (
+        'vendor/lib/libPerfectlyClearCrux.so',
+        'vendor/lib/libSonyIMX376RmscLibrary.so',
+        'vendor/lib/libSonyIMX519PdafLibrary.so',
+        'vendor/lib/libarcsoft_dualcam_bokeh_api.so',
+        'vendor/lib/libarcsoft_dualcam_refocus.so',
+        'vendor/lib/libarcsoft_dualcam_refocus_left.so',
+        'vendor/lib/libarcsoft_dualcam_refocus_preview.so',
+        'vendor/lib/libarcsoft_dualcam_refocus_right.so',
+        'vendor/lib/libarcsoft_smart_denoise.so',
+        'vendor/lib/libdepthmap.so',
+        'vendor/lib/libfilter.so',
+        'vendor/lib/libmmcamera_hdr_gb_lib.so',
+    ): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'RMX1801',
+    'realme',
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
