e9d285b88c7d68c34e776dd505c637f6d5573280
dodyirawan85 <dev.irawans@gmail.com>
2025-07-20 17:19:10 +0700

    RMX1801: Cleanup useless audio passthrough impl
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -90,9 +90,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl:32 \
     android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl:32 \
