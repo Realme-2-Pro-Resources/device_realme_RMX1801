99d7780fb1f470321d7dc796e4bc9e8992c322d4
dodyirawan85 <dev.irawans@gmail.com>
2025-08-02 10:06:23 +0700

    RMX1801: fix vendor.qti.hardware.qccvndhal@1.0 ELF checks
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -43,6 +43,7 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.uceservice@2.3',
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccvndhal@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
