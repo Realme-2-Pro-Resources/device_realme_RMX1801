8b4e2f1d85d114f76598d08f7ac13f75e9ecbfab
dodyirawan85 <dev.irawans@gmail.com>
2025-08-03 09:59:16 +0700

    RMX1801: Drop useless audio v5 fcm
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/oppo/vintf/oppo_framework_compatibility_matrix.xml \
