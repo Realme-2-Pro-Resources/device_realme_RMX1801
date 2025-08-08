08a779ca133aadc940392af0e97b78f8c03f4b85
dodyirawan85 <dev.irawans@gmail.com>
2025-08-08 15:48:42 +0700

    RMX1801: Move lineage-sdk overlay into frameworks overlay
    * Also cleanup and rename to overlay-yaap while we're at it
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
