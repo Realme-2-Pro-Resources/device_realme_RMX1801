28ad3805d6fc5a66692c585a0a6e3ffd66035a4e
LuK1337 <priv.luk@gmail.com>
2025-08-20 19:20:35 -0400

    RMX1801: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Media
 PRODUCT_PACKAGES += \
