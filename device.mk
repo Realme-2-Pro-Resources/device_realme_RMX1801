4b1871af865d0d73dd0880aaec0520dc9ca135e9
dodyirawan85 <dev.irawans@gmail.com>
2025-09-05 09:16:50 +0700

    RMX1801: Switch to aidl powerstats
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -255,7 +255,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats-service.example
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
